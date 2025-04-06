6aeec33bdf32a3fc7c0e570db0835f9bd9e00e26
yurimlee1 <yurim.lee.123@gmail.com>
2025-04-06 13:59:00 -0400

    updated contact section

--- my-app/src/sections/Contact.tsx
+++ my-app/src/sections/Contact.tsx
@@ -24,8 +24,7 @@ export const ContactSection = () => {
             <div>
               <button className="text-white bg-gray-900 inline-flex items-center px-6 h-12 rounded-xl gap-2 w-max border border-gray-900">
                 <span className="font-semibold">
-                  {/* <a href={`mailto:${process.env.EMAIL}`}> */}
-                  <a href={`mailto:yurimlee@nanofry.com`}>
+                  <a href={`mailto:${process.env.EMAIL}`}>
                   Contact Me
                   </a>
                 </span>
